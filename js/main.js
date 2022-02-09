23467bfed737c19469005a7a8ec881fa9f8802f9
jiyoonZ <juneky416@gmail.com>
2022-02-09 20:30:55 +0900

    슬라이드 토글추가

--- js/main.js
+++ js/main.js
@@ -45,4 +45,68 @@ window.addEventListener('scroll', _.throttle(function () {
   }
 }, 300));
 
+
+
 // 요소 순차적으로 나타나기 애니메이션
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach (function (fadeEl, index) {
+   //gsap.to(요소, 지속시간, 옵션{객체데이터~});
+  gsap.to(fadeEl, 1, {
+    delay: (index+1) * .7, //0.7, 1.4, 2.1, 2.8
+    opacity: 1
+  });
+});
+
+// 공지사항 슬라이드
+//Swiper
+// new Swiper (선택자, 옵션)
+new Swiper('.notice-line .swiper', {
+  direction: 'vertical',
+  // autoplay: true,
+  loop: true
+});
+
+//사진 수평 슬라이드
+new Swiper('.promotion .swiper', {
+  //horizontal 이 기본값
+  slidesPerView: 3, //한번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, //1번 슬라이드가 가운데 보이기
+  loop:true, //이걸해야 마지막이후에 첫번째슬라이드가 올수있음
+  // autoplay: {
+  //   delay: 5000
+  // }
+  //페이징 과 화살표
+  pagination : {
+    el: '.promotion .swiper-pagination', // 페이지네이션 선택자
+    clickable: true
+  },
+  navigation: {
+      prevEl: '.promotion .swiper-prev',
+      nextEl: '.promotion .swiper-next'
+  }
+});
+
+// 스타벅스 프로모션 토글
+const promotionEl =document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion')
+let isHidePromotion = false;
+promotionToggleBtn.addEventListener('click', function() {
+  isHidePromotion = !isHidePromotion
+  // isHidePromotion이 true값으로 
+  if(isHidePromotion) {
+    //숨김처리! 
+    promotionEl.classList.add('hide');
+  }else {
+    //보임처리!
+    promotionEl.classList.remove('hide');
+  }
+})
+
+
+
+
+
+
+
+
